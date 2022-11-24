f463a156c3dc3e08d6a2e4dc40b4b2238b62e454
pengrenchang <pengrch@activeio.com.cn>
2022-11-24 10:33:54 +0800

    fix: 初始化更新项目

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+/**
+ * @description:
+ * @author: pengrenchang
+ * @Date: 2022-11-24 10:32:59
+ * @LastEditors: pengrenchang
+ * @LastEditTime: 2022-11-24 10:32:59
+ */
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+/**
+ * @description:
+ * @author: pengrenchang
+ * @Date: 2022-11-24 10:30:28
+ * @LastEditors: pengrenchang
+ * @LastEditTime: 2022-11-24 10:30:28
+ */
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+    plugins: [
+        vue(),
+        Components({
+            resolvers: [AntDesignVueResolver()],
+        }),
+    ],
+});
